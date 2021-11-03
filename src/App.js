d33cda11dd7548111adc2522f5aa3cb65e6343f8
siddharth <siddharth.ashutosh7@gmail.com>
2021-11-03 18:54:22 +0530

    nodeConnector added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useRef, useState} from "react";
+import ForceGraph2D from "react-force-graph-2d";
+import { graphData, maps, links } from "./react_nodalmap_test_data";
 
-function App() {
+const App = () => {
+  const forceRef = useRef();
+  const [highLightNodes, setHighlightNodes] = useState([]);
+  const [highlightLinks, setHighlightLinks] = useState([]);
+
+  function handleNodeClick(node){
+    if(highLightNodes.includes(node)){
+      setHighlightNodes([]);
+      setHighlightLinks([]);
+      return;
+    }
+    var nodeData = [];
+    nodeData.push(node);
+    setHighlightNodes(nodeData);
+
+    var linkData = [];
+    for(let i = 0;i <links.length; i++){
+      if(links[i].source.id === node.id || (links[i].target.id === node.id)){
+        linkData.push(links[i]);
+      }
+    }
+    setHighlightLinks(linkData);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div
+        style={{
+          border: "1px solid gray",
+          marginTop: "20px"
+        }}
+      >
+        <ForceGraph2D
+          width={window.innerWidth}
+          height={window.innerHeight}
+          ref={forceRef}
+          onNodeClick={handleNodeClick}
+          graphData={graphData}
+          cooldownTicks={50}
+          nodeRelSize={7}
+          nodeCanvasObjectMode={() => "after"}
+          nodeCanvasObject={(node, ctx, globalScale) => {
+            const label = maps.get(node.id);
+            const fontSize = 14 /globalScale * 1.2;
+             
+            ctx.font = `${fontSize}px Sans-Serif`;
+            ctx.textAlign = "center";
+            ctx.textBaseline = "middle";
+            ctx.fillStyle = "black";
+            if(node.isPublic){
+              ctx.fillStyle= "orange";
+            } 
+            if (globalScale >= 3.5) {
+              ctx.fillText(label, node.x, node.y + 2.5);
+            }
+          }}
+          linkColor={link => highlightLinks.includes(link) ? "yellow" : "black"}          
+          linkWidth={link => highlightLinks.includes(link) ? 3 : 1}          
+          enableNodeDrag={true}
+        />
+      </div>
     </div>
   );
-}
+};
 
 export default App;
